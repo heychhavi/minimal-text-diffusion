63b03b9d2189bca49352f5c2bddd51263fb31615
chhavi <chhavin6v@gmail.com>
2023-12-28 19:11:06 +0530

    Update gaussian_diffusion.py

--- src/modeling/diffusion/gaussian_diffusion.py
+++ src/modeling/diffusion/gaussian_diffusion.py
@@ -243,7 +243,8 @@ class GaussianDiffusion:
         """
         assert "input_ids" in model_kwargs
         input_ids = model_kwargs.pop("input_ids").to(t.device)
-        x_start_mean = model.model.modules.get_embeds(input_ids)
+        #x_start_mean = model.model.modules.get_embeds(input_ids)
+        x_start_mean = model.model.embed_layer.get_embeds(input_ids)
 
         std = _extract_into_tensor(
             self.sqrt_one_minus_alphas_cumprod,
