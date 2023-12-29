f7dd1ba9765455fd5f30322a47accc8a0bc4ba4c
chhavi <chhavin6v@gmail.com>
2023-12-29 17:04:00 +0530

    Update gaussian_diffusion.py

--- src/modeling/diffusion/gaussian_diffusion.py
+++ src/modeling/diffusion/gaussian_diffusion.py
@@ -258,7 +258,7 @@ class GaussianDiffusion:
         if noise is None:
             noise = th.randn_like(x_start)
         x_t = self.q_sample(x_start, t, noise=noise)  # reparametrization trick.
-        get_logits = model.model.module.get_logits
+        get_logits = model.model.get_logits
 
         terms = {}
 
